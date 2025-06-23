b1700e192d1f80af5e1aff203f67038a67b97dad
Davud <11396@holbertonstudents.com>
2025-06-23 17:24:34 +0400

    0th task classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This script creates a class Square, and creates a private instance attribute __size"""
+class Square:
+    """Defines a square with a private size attribute."""
+    def __init__(self, size):
+        """Initializes the square with a given size."""
+        self.__size = size
